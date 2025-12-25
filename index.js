6c65ab2913168eb486d0cff36c75987c23021106
vineetha <vineethabusireddy@gmail.com>
2025-12-25 14:18:41 +0530

    updated

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const dotenv = require('dotenv');
- 
+
 dotenv.config();
- 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
- 
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [
+        'http://localhost:5173',
+        'https://edutalksacademy.in',
+        'https://www.edutalksacademy.in',
+        process.env.FRONTEND_URL
+    ].filter(Boolean),
     credentials: true
 }));
 app.use(express.json());
- 
+
 // Database connection
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -21,7 +26,7 @@ const db = mysql.createConnection({
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 });
- 
+
 db.connect((err) => {
     if (err) {
         console.error('Database connection failed: ' + err.stack);
@@ -29,23 +34,23 @@ db.connect((err) => {
     }
     console.log('Connected to MySQL database as ID ' + db.threadId);
 });
- 
+
 // Routes
 const contactRoutes = require('./routes/contactRoutes');
 const newsletterRoutes = require('./routes/newsletterRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const jobRoutes = require('./routes/jobRoutes');
- 
+
 app.use('/api/contact', contactRoutes);
 app.use('/api/newsletter', newsletterRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/jobs', jobRoutes);
- 
+
 // Basic Route
 app.get('/', (req, res) => {
     res.send('Edutalks Portfolio API is running...');
 });
- 
+
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
