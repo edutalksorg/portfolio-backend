42851702ff67216fee077465a5cd8fc1b502a618
vineetha <vineethabusireddy@gmail.com>
2025-12-25 14:57:58 +0530

    latest updates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
-        'http://localhost:5173',
+
         'https://edutalksacademy.in',
         'https://www.edutalksacademy.in',
         process.env.FRONTEND_URL
